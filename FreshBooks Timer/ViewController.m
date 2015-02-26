82ebf65f3e7dffd8bb4d52c289ace7ae38a1c322
Ryan Hoshor <russianryebread@gmail.com>
2015-02-26 09:54:59 -0600

    First Commit

--- FreshBooks Timer/ViewController.m
+++ FreshBooks Timer/ViewController.m
@@ -12,14 +12,37 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
-    // Do any additional setup after loading the view.
+    
+    [self loadCookies];
+    [_webView setMainFrameURL:@"https://farmsoftstudios.freshbooks.com/internal/timesheet/timer"];
 }
 
 - (void)setRepresentedObject:(id)representedObject {
     [super setRepresentedObject:representedObject];
 
     // Update the view, if already loaded.
+}
+
+- (void)saveCookies
+{
+    NSData         *cookiesData = [NSKeyedArchiver archivedDataWithRootObject: [[NSHTTPCookieStorage sharedHTTPCookieStorage] cookies]];
+    NSUserDefaults *defaults    = [NSUserDefaults standardUserDefaults];
+    [defaults setObject: cookiesData forKey:@"cookies"];
+    [defaults synchronize];
+}
+
+- (void)loadCookies
+{
+    NSArray             *cookies       = [NSKeyedUnarchiver unarchiveObjectWithData: [[NSUserDefaults standardUserDefaults] objectForKey: @"cookies"]];
+    NSHTTPCookieStorage *cookieStorage = [NSHTTPCookieStorage sharedHTTPCookieStorage];
+    
+    for (NSHTTPCookie *cookie in cookies) {
+        [cookieStorage setCookie: cookie];
+    }
+}
+
+-(void)viewDidDisappear {
+    [self saveCookies];
 }
 
 @end
